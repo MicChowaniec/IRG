886b6d162dde216276194d3dd85dfcf1c99b5797
MicChowaniec <micchowaniec@gmail.com>
2024-03-13 01:28:07 +0100

    Honeycomb map initiator

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,8 +7,9 @@ public class GameManager : MonoBehaviour
     public GameObject Field;
     public GameObject Plane;
     private Vector3 locationOfPlane;
-    public int size;
-    
+    public int mapsize;
+    public float gaps;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,15 +23,92 @@ public class GameManager : MonoBehaviour
     }
     void CreateMap()
     {
-        Field.transform.position=locationOfPlane;
-        for(int i = 0; i < size; i++)
-        {
-            
-            locationOfPlane = new Vector3(i,0,i);
-            Instantiate(Field);
-        }
         
+       if(mapsize>=1)
+        {
+            locationOfPlane=Vector3.zero;
+            InstantiateField();
+        }
+        if (mapsize >=2)
+        {
+            TransformCircle2();
+        }
+        if (mapsize >= 3)
+        {
+            TransformCircle3();
+        }
 
+
+    }
+    void TransformCircle2()
+    {
+        //1st node
+        locationOfPlane.x = 0;
+        locationOfPlane.z = gaps;
+        InstantiateField();
+
+        //2nd node
+        locationOfPlane.z = gaps / 2;
+        locationOfPlane.x = gaps * Mathf.Sqrt(3) / 2;
+        InstantiateField();
+
+        //3rd node
+        locationOfPlane.z = -gaps / 2;
+        InstantiateField();
+
+        //4th node
+        locationOfPlane.x = 0;
+        locationOfPlane.z = -gaps;
+        InstantiateField();
+
+        //5th node
+        locationOfPlane.z = -gaps / 2;
+        locationOfPlane.x = -gaps * Mathf.Sqrt(3)/2;
+        InstantiateField();
+
+        //6th node
+        locationOfPlane.z = gaps / 2;
+        InstantiateField();
+
+    }
+
+    void TransformCircle3()
+    {
+        //1st node
+        locationOfPlane.x = 0;
+        locationOfPlane.z = 2*gaps;
+        InstantiateField();
+
+        //2nd node
+        locationOfPlane.x = 2*gaps / 2;
+        locationOfPlane.z = 2 * gaps / Mathf.Sqrt(3);
+        InstantiateField();
+
+        //3rd node
+        locationOfPlane.x = -2*gaps / 2;
+        InstantiateField();
+
+        //4th node
+        locationOfPlane.x = 0;
+        locationOfPlane.z = -2*gaps;
+        InstantiateField();
+
+        //5th node
+        locationOfPlane.x = -2*gaps / 2;
+        locationOfPlane.z = -2*gaps / Mathf.Sqrt(3);
+        InstantiateField();
+
+        //6th node
+        locationOfPlane.x = -2*gaps / 2;
+        locationOfPlane.z = 2*gaps / Mathf.Sqrt(3);
+        InstantiateField();
+
+    }
+    void InstantiateField()
+    {
+        
+        Field.transform.position = locationOfPlane;
+        Instantiate(Field);
     }
 
 }
