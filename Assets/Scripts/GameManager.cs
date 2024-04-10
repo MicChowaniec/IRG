855bffb247b24e4ceeb1f4bfa714aa0a6e139ef5
MicChowaniec <micchowaniec@gmail.com>
2024-04-10 23:09:39 +0200

    MapCreatorRefactored

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering.UI;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject fieldTile;
     public int mapSize;
+    public Vector3 fieldPosition;
+    public GameObject mapAnchor;
+    public float gaps;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +25,16 @@ public class GameManager : MonoBehaviour
 
     void CreateMap()
     {
-
+        for (int i=-mapSize+1; i<mapSize; i++)
+        {
+            
+            for (int j = -mapSize + 1; j < (mapSize - Mathf.Abs(i)); j++)
+            {
+                
+                fieldPosition = new Vector3(((float)i*Mathf.Sqrt(3)/2)*gaps, 0, ((float)j+Mathf.Abs((float)i)/2) * gaps );
+                Instantiate(fieldTile, fieldPosition, Quaternion.identity, mapAnchor.transform);
+            }
+        }
     }
 
 }
