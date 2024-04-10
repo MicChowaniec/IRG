28612c6f21604b2bb7b868d7318818e7cf8510b5
MicChowaniec <micchowaniec@gmail.com>
2024-04-10 22:26:52 +0200

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject fieldTile;
+    public int mapSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateMap();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateMap()
+    {
+
+    }
+
+}
