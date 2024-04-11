a4d994eef3eb010c2f80958ffff97a981c6e7e24
MicChowaniec <micchowaniec@gmail.com>
2024-04-11 22:47:41 +0200

    Adding Materials Refactored

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,10 +11,25 @@ public class GameManager : MonoBehaviour
     public Vector3 fieldPosition;
     public GameObject mapAnchor;
     public float gaps;
+
+    public Material sandMaterial;
+    public Material shallowWaterMaterial;
+    public Material grassMaterial;
+    public Material dirtMaterial;
+    public Material rockMaterial;
+    public Material deepWaterMaterial;
+
+    private Material[] Materials = new Material[5] ;
     // Start is called before the first frame update
     void Start()
     {
+        Materials[0] = sandMaterial;
+        Materials[1] = shallowWaterMaterial;
+        Materials[2] = grassMaterial;
+        Materials[3] = dirtMaterial;
+        Materials[4] = rockMaterial;
         CreateMap();
+        
     }
 
     // Update is called once per frame
@@ -30,9 +45,11 @@ public class GameManager : MonoBehaviour
             
             for (int j = -mapSize + 1; j < (mapSize - Mathf.Abs(i)); j++)
             {
-                
-                fieldPosition = new Vector3(((float)i*Mathf.Sqrt(3)/2)*gaps, 0, ((float)j+Mathf.Abs((float)i)/2) * gaps );
-                Instantiate(fieldTile, fieldPosition, Quaternion.identity, mapAnchor.transform);
+                int rand = Random.Range(0, Materials.Length);
+                Debug.Log(rand);
+                fieldPosition = new Vector3(((float)i*Mathf.Sqrt(3)/2)*gaps, 0, ((float)j+Mathf.Abs((float)i)/2) * gaps);
+                GameObject newFieldTile = Instantiate(fieldTile, fieldPosition, Quaternion.identity, mapAnchor.transform);
+                newFieldTile.GetComponent<MeshRenderer>().material = Materials[rand];
             }
         }
     }
